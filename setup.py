2761a9402bb1311b998f8d4951426e00957189ee
Rahul <saihanumarahul66@gmail.com>
2023-07-23 11:16:15 +0530

    updated layout
    created basic cli
    working on exporting the dashboard

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WDIO-PY',
+    version='0.1.0',
+    packages=['src'],
+    url='https://github.com/RahulARanger/wdio-next-python',
+    license='MIT License',
+    author='Rahul A Ranger',
+    author_email='saihanumarahul66@gmail.com',
+    description='Utilizing Nextjs for dashboard and python for test result processing'
+)
