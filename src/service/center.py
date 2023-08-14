2b5a37b7542304c6e7eb77843b5185ffec37822a
Rahul <saihanumarahul66@gmail.com>
2023-08-15 00:44:31 +0530

    Handled lifecycle of the database
    Working on the background tasks

--- src/service/center.py
+++ src/service/center.py
@@ -1,9 +1,13 @@
 from sanic import Sanic
 from sanic.response import text, json, JSONResponse, HTTPResponse
 from sanic.request import Request
-from tortoise.contrib.sanic import register_tortoise
-from src.service.oneliners import one_liners, save_file_name
+from tortoise import Tortoise, connections
+from src.service.oneliners import one_liners
 from src.service.DBService.center import service
+from pathlib import Path
+from tempfile import mkstemp
+from os import close
+from multiprocessing.sharedctypes import Array
 from src.service.DBService.getThings import get_service
 
 service_provider = Sanic("WDIO-PY")
@@ -22,6 +26,45 @@ def isItDone(request: Request) -> JSONResponse:
     return json({"done": True})
 
 
-register_tortoise(service_provider, db_url=r"{}".format(f'sqlite://${save_file_name}'), modules={
-    "models": ["src.service.DBService.models"]
-}, generate_schemas=True)
+async def init_tortoise_orm(file_path: str):
+    await Tortoise.init(db_url=r"{}".format(f'sqlite://{file_path}'), modules={
+        "models": ["src.service.DBService.models"]
+    })
+    await Tortoise.generate_schemas()
+
+
+# from tortoise.contrib.sanic import register_tortoise
+# custom way to register tortoise, we are doing this because the file path is dynamically generated
+@service_provider.main_process_start
+async def started(app, loop):
+    _, temp_file = mkstemp(suffix=".db", prefix='wdio-py-reporter')
+    close(_)
+    # temp_file = "sample.db"
+    app.shared_ctx.db_path = Array('c', str.encode(temp_file), lock=False)
+
+
+@service_provider.before_server_start
+async def before_start_of_day(app: Sanic, loop):
+    file_path = app.shared_ctx.db_path.value.decode("utf-8")
+    print("starting connection", file_path)
+    await init_tortoise_orm(file_path)
+
+
+@service_provider.after_server_stop
+async def end_of_day(app: Sanic, loop):
+    print("closing connections")
+    await connections.close_all()
+
+
+@service_provider.main_process_stop
+async def packing_bags(app: Sanic, loop):
+    try:
+        main_file = Path(app.shared_ctx.db_path.value.decode("utf-8"))
+        shm = main_file.parent / (main_file.name + "-shm")
+        wal = main_file.parent / (main_file.name + "-wal")
+        shm.unlink(missing_ok=True)
+        wal.unlink(missing_ok=True)
+        main_file.unlink(missing_ok=True)
+        print("DELETED DB")
+    except OSError as error:
+        print("FAILED TO DELETE DB", error)
--- src/service/oneliners.py
+++ src/service/oneliners.py
@@ -2,19 +2,6 @@ from sanic.blueprints import Blueprint
 from sanic.response import HTTPResponse, text
 from sanic.request import Request
 from typing import TypedDict
-from tempfile import TemporaryFile
-from pathlib import Path
-from sanic.log import logger
-
-
-temp_file = TemporaryFile(suffix=".db")
-save_file_name = f'/{temp_file.name}'
-logger.info("Connecting to a database at: %s", save_file_name)
-
-del temp_file
-
-# save_file_name = "sample-test.sqlite"
-# uncomment above line if you need to debug the file generated
 
 one_liners = Blueprint(name="one_liners", url_prefix="/")
 
@@ -32,13 +19,7 @@ class ByeWithCommands(TypedDict):
 
 # bye is core request but can sometimes be cruel if it didn't complete its work
 @one_liners.post("/bye")
-def bye(request: Request) -> HTTPResponse:
+async def bye(request: Request) -> HTTPResponse:
     resp: ByeWithCommands = request.json
-
-    try:
-        Path(save_file_name).unlink() if resp.get("deleteDB", True) else None
-    except OSError:
-        logger.exception("Failed to delete the database", exc_info=True)
-
     request.app.m.terminate()
     return text("1")
