28d073f4d860e49c852a2529a86e8ff53c1cd7c7
Rahul <saihanumarahul66@gmail.com>
2023-08-21 09:51:08 +0530

    changed the path of the next-py-dashboard
    to include it in the wheel

--- src/shipment.py
+++ src/shipment.py
@@ -28,7 +28,7 @@ class Shipment:
 
     @property
     def dashboard(self):
-        return Path(__file__).parent.parent / "next-py-dashboard"
+        return Path(__file__).parent / "next-py-dashboard"
 
     def verify_cache(self) -> bool:
         return (self.cache / "package.json").exists() and \
