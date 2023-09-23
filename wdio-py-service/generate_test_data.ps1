e8ae280391955fb55280918480e81c23a427537d
Rahul <saihanumarahul66@gmail.com>
2023-09-23 18:21:59 +0530

    init wdio-py-service

--- wdio-py-service/generate_test_data.ps1
+++ wdio-py-service/generate_test_data.ps1
@@ -1,12 +1,5 @@
 npm run test:mocha
 npm run test:cucumber
-npm run test:mocha
-npm run test:cucumber
-npm run test:mocha
-npm run test:cucumber
-npm run test:mocha
-npm run test:cucumber
-npm run test:mocha
 cd ..
 ./venv/scripts/activate
 next-py patch ./TestResults
