d92eb4fa34c973dbb94d7a63901ce048eb9ec0fa
Rahul <saihanumarahul66@gmail.com>
2023-11-17 23:05:11 +0530

    test: mapped correct output path for executable feat: storing the executable

--- __test__/test_sanity/test_cli.py
+++ __test__/test_sanity/test_cli.py
@@ -1,3 +1,5 @@
+import platform
+
 from pytest import mark
 from __test__.test_regression.test_utils import TestSetConfigCommand
 
@@ -5,7 +7,20 @@ from __test__.test_regression.test_utils import TestSetConfigCommand
 @mark.usefixtures("clean_close")
 class TestCli:
     def test_dist_existence(self, dist):
-        assert dist.exists()
+        possible_location = dist
+
+        target = platform.system()
+
+        if target == "Windows":
+            possible_location /= "graspit.exe"
+        elif target == "Linux":
+            possible_location /= "graspit"
+        elif target == "Darwin":
+            possible_location /= "graspit.app"
+        else:
+            assert False, f"{target} is not supported"
+
+        assert possible_location.exists() is True, "Exe was not found"
 
     async def test_set_config_with_one_para(self, root_dir, dist):
         return await TestSetConfigCommand().test_set_config_with_one_para(
