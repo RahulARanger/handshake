006989187267dbadff15c3cc188af9b889786be8
Rahul <saihanumarahul66@gmail.com>
2023-11-30 00:24:00 +0530

    feature: disabled timeline feat: migrated to echarts

--- graspit-reporters/packages/graspit-dashboard/src/components/Charts/constants.ts
+++ graspit-reporters/packages/graspit-dashboard/src/components/Charts/constants.ts
@@ -1,4 +1,3 @@
-import type { GradientColorObject } from 'highcharts';
 import { REM } from 'next/font/google';
 import * as echarts from 'echarts/core';
 import type { TooltipComponentOption } from 'echarts/components';
@@ -8,35 +7,6 @@ export const serif = REM({
     weight: '300',
     adjustFontFallback: true,
 });
-export const skippedGradient: GradientColorObject = {
-    linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-    stops: [
-        [0, 'rgb(255, 255, 0)'], // Lightest Custom Yellow
-        [0.4, 'rgb(204, 204, 0)'], // Medium Custom Yellow
-        [0.7, 'rgb(153, 153, 0)'], // Dark Custom Yellow
-        [0.9, 'rgb(102, 102, 0)'], // Darkest Custom Yellow
-    ],
-};
-
-export const redGradient: GradientColorObject = {
-    linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-    stops: [
-        [0, 'rgb(255, 0, 0)'], // Lightest Custom Color
-        [0.4, 'rgb(204, 0, 0)'], // Medium Cus  tom Color
-        [0.7, 'rgb(153, 0, 0)'], // Dark Custom Color
-        [0.9, 'rgb(102, 0, 0)'], // Darkest Custom Color
-    ],
-};
-
-export const greenGradient: GradientColorObject = {
-    linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-    stops: [
-        [0, 'rgb(0, 255, 0)'], // Lightest Custom Green
-        [0.4, 'rgb(0, 204, 0)'], // Medium Custom Green
-        [0.7, 'rgb(0, 153, 0)'], // Dark Custom Green
-        [0.9, 'rgb(0, 102, 0)'], // Darkest Custom Green
-    ],
-};
 
 export const radiantGreen = new echarts.graphic.LinearGradient(0, 0, 0, 1, [
     { offset: 0, color: 'rgb(0, 102, 0)' }, // Lighter shade at the top
