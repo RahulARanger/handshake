c5d1adba0de455e932bfd1db17ef7e68661aaa5f
Rahul <saihanumarahul66@gmail.com>
2023-12-16 19:38:05 +0530

    removed old files

--- handshake-nodejs-reporters/packages/graspit-dashboard/src/components/utils/Datetime/format.tsx
+++
@@ -1,3 +0,0 @@
-export const dateTimeFormatUsed = "MMM, ddd Do YYYY kk:mm:ss A";
-export const dateFormatUsed = "MMM, ddd Do YYYY";
-export const timeFormatUsed = "HH:mm:ss A";
--- handshake-nodejs-reporters/packages/wdio-graspit-reporter/src/index.ts
+++
@@ -1,7 +0,0 @@
-import GraspItService from './service';
-import HandshakeReporter from './reporter';
-
-export default HandshakeReporter;
-export * from './types';
-export const launcher = GraspItService;
-export * from './helpers';
