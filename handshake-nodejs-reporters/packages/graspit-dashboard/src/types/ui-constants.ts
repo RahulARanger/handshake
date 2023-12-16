040447b3ae36eb7928790f2ea4b29415483c7cea
Rahul <saihanumarahul66@gmail.com>
2023-12-16 19:38:05 +0530

    removed old files

--- handshake-nodejs-reporters/packages/graspit-dashboard/src/types/ui-constants.ts
+++
@@ -1,6 +0,0 @@
-export const gridViewMode = 'grid';
-export const treeViewMode = 'tree';
-
-export const testEntitiesTab = 'test-entities';
-export const overviewTab = 'overview';
-export const timelineTab = 'timelineTab';
