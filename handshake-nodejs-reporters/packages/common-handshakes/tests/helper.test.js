6532487decb83402605cdff585427323cf2da6ed
Rahul <saihanumarahul66@gmail.com>
2023-12-17 20:20:48 +0530

    fix: path issue in helper.test.js

--- handshake-nodejs-reporters/packages/common-handshakes/tests/helper.test.js
+++ handshake-nodejs-reporters/packages/common-handshakes/tests/helper.test.js
@@ -1,14 +1,16 @@
 const { describe, test, expect } = require('@jest/globals');
+const { join } = require('path');
 const { sanitizePaths, acceptableDateString } = require('../src/helpers');
 
 describe('verifying sanitizer', () => {
+  const current = join('tests', 'helper.test.js');
   test('verifying the basic case', () => {
-    expect(sanitizePaths([__filename])).toEqual(['tests\\helper.test.js']);
-    expect(sanitizePaths([__filename, __dirname])).toEqual(['tests\\helper.test.js', 'tests']);
+    expect(sanitizePaths([__filename])).toEqual([current]);
+    expect(sanitizePaths([__filename, __dirname])).toEqual([current, 'tests']);
   });
 
   test('with file://', () => {
-    expect(sanitizePaths([`file:///${__filename}`, `file:///${__dirname}`])).toEqual(['tests\\helper.test.js', 'tests']);
+    expect(sanitizePaths([`file:///${__filename}`, `file:///${__dirname}`])).toEqual([current, 'tests']);
   });
 
   test('convert the date to iso string', () => {
