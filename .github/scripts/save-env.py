001ab57029bc493a57e936b0be47ad88a08222be
Rahul <saihanumarahul66@gmail.com>
2024-02-18 19:36:22 +0530

    fix: build .env before build dist

--- .github/scripts/save-env.py
+++ .github/scripts/save-env.py
@@ -3,4 +3,4 @@ from pathlib import Path
 
 # assuming you are in the root of the project
 
-(Path.cwd() / "handshake" / ".env").write_text(getenv("PROD_ENV"))
+(Path.cwd() / "handshake" / ".env").write_text(getenv("ENV_FILE"))
