e8a39af106bcb8c346e290fa7bfa6bac285971f2
Rahul <saihanumarahul66@gmail.com>
2024-03-14 00:00:31 +0530

    bump to 0.3.2 fix: server termination fix: workers configuration

--- handshake/services/Endpoints/center.py
+++ handshake/services/Endpoints/center.py
@@ -1,15 +1,13 @@
 from handshake.services.Endpoints.core import service_provider
 from handshake.services.DBService.lifecycle import init_tortoise_orm, close_connection
 from handshake.services.DBService.shared import set_test_id
-import asyncio
-from signal import signal, SIGTERM, SIGINT
-from sanic import Sanic
 
 
 @service_provider.before_server_start
 async def before_start_of_day(*args):
     set_test_id()
     await init_tortoise_orm()
+
 
 @service_provider.after_server_stop
 async def close_app(*args):
