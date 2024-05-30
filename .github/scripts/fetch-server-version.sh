7cf87ca129273d51e2cfcfea9ecd51d17f72825d
Rahul <saihanumarahul66@gmail.com>
2024-05-31 00:51:57 +0530

    fix: release build file for the common-handshakes

--- .github/scripts/fetch-server-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# root directory is in root of this project
-
-file=$(<"handshake/__init__.py");
-
-
-output=$(echo "$file" | grep -Po "__version__ = \"(.*)\"" | grep -Po "([0-9\.]*)" | xargs)
-echo "VERSION=$output" >> "$GITHUB_OUTPUT"
-
--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,27 @@ from pathlib import Path
 from shutil import make_archive, move
 
 
+def save_version_to_version_file():
+    root = pathlib.Path(__file__).parent
+    target_node = (
+        root
+        / "handshake-nodejs-reporters"
+        / "packages"
+        / "common-handshakes"
+        / ".version"
+    )
+    assert target_node.exists()
+    version_text = tomllib.loads((root / "pyproject.toml").read_text())["tool"][
+        "poetry"
+    ]["version"]
+    note_file = root / "handshake" / "__init__.py"
+    note_file.write_text(f'__version__ = "{version_text}"\n')
+
+    prev = dict(**loads(target_node.read_text()))
+    prev.pop("version")
+    target_node.write_text(dumps(dict(**prev, version=version_text)))
+
+
 @nox.session
 def version(session):
     """
@@ -16,27 +37,20 @@ def version(session):
     """
     bump = session.posargs[0].lower()
     assert bump in {"minor", "major", "patch"}
-    root = pathlib.Path(__file__).parent
-    target_node = (
-        root
-        / "handshake-nodejs-reporters"
-        / "packages"
-        / "common-handshakes"
-        / ".version"
-    )
-    assert target_node.exists()
 
     session.run("poetry", "version", bump, external=True)
+    save_version_to_version_file()
 
-    version_text = tomllib.loads((root / "pyproject.toml").read_text())["tool"][
-        "poetry"
-    ]["version"]
-    note_file = root / "handshake" / "__init__.py"
-    note_file.write_text(f'__version__ = "{version_text}"\n')
 
-    prev = dict(**loads(target_node.read_text()))
-    prev.pop("version")
-    target_node.write_text(dumps(dict(**prev, version=version_text)))
+@nox.session
+def save_version(session):
+    """
+    saves the version and saves it to the required places
+    example: nox -s save_version
+    :param session: nox session
+    :return:
+    """
+    save_version_to_version_file()
 
 
 @nox.session(python=["3.11", "3.12"])
